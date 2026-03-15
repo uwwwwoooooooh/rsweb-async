42420c8679b10adfc1cdcd9ddce60b1d31c168af
will <konatan2805@gmail.com>
2026-03-15 22:09:19 +0800

    feature: async

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
 use std::{
     fs,
+    time::Duration,
+};
+use async_std::{
+    task,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
+use futures::stream::StreamExt;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    // for stream in listener.incoming() {
+    //     let stream = stream.unwrap();
 
-        handle_connection(stream);
-    }
+    //     handle_connection(stream).await;
+    // }
+
+    listener
+        .incoming()
+        .for_each_concurrent(None, |stream| async {
+            let stream = stream.unwrap();
+            handle_connection(stream).await;
+        })
+        .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader.lines().next().await.unwrap().unwrap();
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if request_line == "GET /slow HTTP/1.1" {
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -30,5 +47,5 @@ fn handle_connection(mut stream: TcpStream) {
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
